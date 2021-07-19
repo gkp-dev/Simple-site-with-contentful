e57415e594bfebc826b58ea79b3e0919fb068b07
Gankpe Jean-loris <gkprofessionel@gmail.com>
2021-07-19 14:05:49 +0200

    add 404 page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import { createClient } from 'contentful'
 import RecipeCard from '../components/RecipeCard'
 
 export default function Recipes({ recipes }) {
-  console.log(recipes)
   return (
     <div className="recipe-list">
       {recipes.map((recipe) => (
