e64da72bdd3391cc7ab9b924b0b69af78a92eb34
Gankpe Jean-loris <gkprofessionel@gmail.com>
2021-07-19 10:55:10 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,38 @@
-export default function Recipes() {
+import { createClient } from 'contentful'
+import RecipeCard from '../components/RecipeCard'
+
+export default function Recipes({ recipes }) {
+  console.log(recipes)
   return (
-    <div className="recipe-list">
-      Recipe List
+    <div className="recipe-list" >
+      {recipes.map(recipe => (
+        <RecipeCard key={recipe.sys.id} recipe={recipe}></RecipeCard>
+      ))}
+
+      <style jsx>{`
+        .recipe-list {
+          display:grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap:20px;
+        }
+      `}</style>
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const {CONTENTFUL_SPACE_ID,CONTENTFUL_ACCESS_TOKEN} = process.env
+  const client = createClient({
+    space: CONTENTFUL_SPACE_ID,
+    accessToken: CONTENTFUL_ACCESS_TOKEN
+  })
+
+  const res = await client.getEntries({ content_type: 'recipe' })
+  
+  return {
+    props: {
+      recipes: res.items
+    }
+  }
+  
 }
